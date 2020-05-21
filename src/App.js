f67b175c91843398c388670b12b25110fe3beae9
Vrishank <vrishank98@gmail.com>
2020-05-21 23:40:42 +0530

    Submission
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Faker from 'faker'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      users: [] ,
+    }
+  }
+
+
+  componentDidMount() {
+    for (let i = 0; i < 15; i++) {
+      const user = {
+        name: Faker.name.firstName(),
+        email: Faker.internet.email(),
+        avatar: "https://picsum.photos/100/100?random=" + Math.floor(Math.random() * 100),
+        description: Faker.name.jobDescriptor(),
+        city: Faker.address.city(),
+        streetName: Faker.address.streetName(),
+        country: Faker.address.country(),
+        company: Faker.company.companyName(),
+      }
+      this.setState(prevState => ({
+        users: [...prevState.users, user],
+      }))
+    }
+  }
+
+
+  getUsers(user) {
+    return (
+      <div style={{ padding: '10px' ,border: 'solid 1px #eee'}}>
+        <img src={user.avatar} alt={user.name} width="100" height="100" />
+        <h4>Name: {user.name}</h4>
+        <h4>Email: {user.email}</h4>
+        <h4>Description: {user.description}</h4>
+        <h4>Address: {user.streetName}, {user.city}, {user.country}</h4>
+        <h4>Company: {user.company}</h4>
+      </div>
+    )
+  }
+
+  render() {
+    return <div>{this.state.users.map(user => this.getUsers(user))}</div>
+  }
 }
 
 export default App;
